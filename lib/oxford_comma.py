d36a8f09fc805120865066a20a7139e977ebe971
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-03-28 21:08:01 -0400

    All tests passing. Understanding the ''.join() operator better

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        # concatenate all items that aren't the last with an oxford comma.
+        # concatenate the last item with ' and ' 
+        return ', '.join(items[:-1]) + f', and {items[-1]}'
+
+if __name__ == "__main__":
+    print(oxford_comma(["sex","rock and roll"]))
